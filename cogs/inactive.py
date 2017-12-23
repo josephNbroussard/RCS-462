934fa60ade2ff885337c2f78e69ccd9c9cc8f52a
jensolaf <jens@animal.ee>
2017-12-23 03:29:58 +0200

    Changed py version from 3.4 to 3.5+

--- cogs/inactive.py
+++ cogs/inactive.py
@@ -18,20 +18,17 @@ class Inactivity:
 
     @commands.command(name='forcecheck')
     @commands.guild_only()
-    @asyncio.coroutine
-    def cmd_forcecheck(self, ctx):
+    async def cmd_forcecheck(self, ctx):
         """Force a check for inactive users."""
         return
 
     @commands.command(name='inactiverole')
     @commands.guild_only()
-    @asyncio.coroutine
-    def cmd_inactiverole(self, ctx, discord):
+    async def cmd_inactiverole(self, ctx, discord):
         """Force a check for inactive users."""
         return
 
-    @asyncio.coroutine
-    def on_message(self, ctx):
+    async def on_message(self, ctx):
         if not db.if_member_in_db(ctx.guild,ctx.author):
             db.member_add_db(ctx.guild,ctx.author)
             #yield from ctx.guild.default_channel.send('Welcome!')
@@ -40,8 +37,5 @@ class Inactivity:
         #print(ctx.author)
 
 
-    
-# The setup fucntion below is neccesarry. Remember we give bot.add_cog() the name of the class in this case SimpleCog.
-# When we load the cog, we use the name of the file.
 def setup(bot):
     bot.add_cog(Inactivity(bot))
--- cogs/statistics.py
+++ cogs/statistics.py
@@ -20,15 +20,14 @@ def setup(bot):
     @commands.command(name='recount')
     @commands.check(check_admin)
     @commands.guild_only()
-    @asyncio.coroutine
-    def cmd_partyadd(self, ctx, party_role: discord.Role=None, party_leader_role: discord.Role=None, channel: discord.TextChannel=None):
+    async def cmd_partyadd(self, ctx, party_role: discord.Role=None, party_leader_role: discord.Role=None, channel: discord.TextChannel=None):
         """Add a party into the Database."""
         guild = ctx.guild
         ch = ctx.channel
         try:
             
             if party_role is None or party_leader_role is None or channel is None:
-                yield from ch.send(embed=status.wrong_input("Did you provide role, leader role and channel?"))
+                await ch.send(embed=status.wrong_input("Did you provide role, leader role and channel?"))
                 return
 
             print(party_leader_role.name)
@@ -37,14 +36,14 @@ def setup(bot):
 
             db.new_party(guild, party_role.id, party_leader_role.id, str(party_role.name).split(" (", 1)[0], party_role.color, channel.id)
             print("lol")
-            yield from ch.send(embed=status.cmd_partyadd(party_role.mention, party_leader_role.mention, party_role.color, channel.mention))
+            await ch.send(embed=status.cmd_partyadd(party_role.mention, party_leader_role.mention, party_role.color, channel.mention))
             
             
             print("Added a new party.")
             
         except Exception as e:
             try:   
-                yield from ch.send(embed=status.error(e))
+                await ch.send(embed=status.error(e))
                 print(e)
             except:
                 print(e)
